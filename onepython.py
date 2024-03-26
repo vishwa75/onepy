e43ecfd478596a472f1a70776280bf55d5207f7a
vishwa75 <vishwak757@gmail.com>
2024-03-27 02:42:40 +0530

    send mail

--- onepython.py
+++ onepython.py
@@ -1,7 +1,7 @@
 import datetime
 
 now = datetime.datetime.now()
-print("Hiii build test neww  The current date and time is:", now)
+print("Hiii build test neww send mail  The current date and time is:", now)
 
 with open("data.txt", "w") as f:
     f.write("This file was generated on " + str(now) + "\n")
