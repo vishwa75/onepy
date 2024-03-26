2970bf797d3e7cf2f54406e4b9d015116353feb4
vishwa75 <vishwak757@gmail.com>
2024-03-26 15:09:05 +0000

    add hiiii

--- onepython.py
+++ onepython.py
@@ -1,7 +1,7 @@
 import datetime
 
 now = datetime.datetime.now()
-print("The current date and time is:", now)
+print("Hiii The current date and time is:", now)
 
 with open("data.txt", "w") as f:
     f.write("This file was generated on " + str(now) + "\n")
