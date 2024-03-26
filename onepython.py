ebf897c771d1f481dd6397d1ddcf73bea05203fd
vishwa75 <vishwak757@gmail.com>
2024-03-26 16:53:20 +0000

    jenkins build test

--- onepython.py
+++ onepython.py
@@ -1,7 +1,7 @@
 import datetime
 
 now = datetime.datetime.now()
-print("Hiii The current date and time is:", now)
+print("Hiii build test The current date and time is:", now)
 
 with open("data.txt", "w") as f:
     f.write("This file was generated on " + str(now) + "\n")
